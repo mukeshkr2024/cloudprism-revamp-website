2e7c446d01dcef18c211aa220c0f4f30a31a1657
mukesh kumar <mkmehta2041@gmail.com>
2024-03-07 19:06:33 +0530

    add minor changes

---
+++ constants/index.ts
@@ -0,0 +1,148 @@
+import { FooterLinks, NavbarLinks, OurTeams } from "@/types";
+
+export const navbarLinks: NavbarLinks[] = [
+  {
+    route: "/about",
+    label: "About Us",
+  },
+  {
+    route: "/services",
+    label: "Services",
+  },
+  {
+    route: "/blogs",
+    label: "Blogs",
+  },
+  {
+    route: "/events",
+    label: "Events",
+  },
+  {
+    route: "/carreers",
+    label: "Careers",
+  },
+];
+
+export const ourTeams: OurTeams[] = [
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+  {
+    name: "Akash Umang",
+    description: "UI/UX Designer",
+    profilePic: "/assets/images/team-1.svg",
+    linkdein: "https",
+  },
+];
+
+export const successStory = [
+  {
+    title: "Reinvention in the age of generative AI",
+    imgUrl: "/assets/images/success-story-1.svg",
+  },
+  {
+    title: "Reinvention in the age of generative AI",
+    imgUrl: "/assets/images/success-story-2.svg",
+  },
+  {
+    title: "Reinvention in the age of generative AI",
+    imgUrl: "/assets/images/success-story-3.svg",
+  },
+  {
+    title: "Reinvention in the age of generative AI",
+    imgUrl: "/assets/images/success-story-4.svg",
+  },
+];
+
+export const footerLinks: FooterLinks[] = [
+  {
+    route: "/about",
+    label: "About",
+  },
+  {
+    route: "/services",
+    label: "Services",
+  },
+  {
+    route: "/blogs",
+    label: "Blogs",
+  },
+  {
+    route: "/events",
+    label: "Events",
+  },
+  {
+    route: "/careers",
+    label: "Careers",
+  },
+];
+
+export const usefullLinks: FooterLinks[] = [
+  {
+    label: "Useful Links",
+    route: "/usefull-link",
+  },
+  {
+    label: "Useful Links",
+    route: "/usefull-link",
+  },
+  {
+    label: "Useful Links",
+    route: "/usefull-link",
+  },
+  {
+    label: "Useful Links",
+    route: "/useful-link",
+  },
+];
---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+export interface NavbarLinks {
+  route: string;
+  label: string;
+}
+export interface OurTeams {
+  name: string;
+  description: string;
+  profilePic: string;
+  linkdein: string;
+}
+
+export interface SucessStory {
+  tittle: string;
+  imgUrl: string;
+}
+
+export interface FooterLinks {
+  route: string;
+  label: string;
+}
