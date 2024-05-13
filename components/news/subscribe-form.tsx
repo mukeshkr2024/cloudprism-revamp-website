3409f217a53d3d722f3d8282c9281f71f0057082
mukesh kumar <mkmehta2041@gmail.com>
2024-05-13 11:22:20 +0530

    minor fixes

--- components/news/subscribe-form.tsx
+++ components/news/subscribe-form.tsx
@@ -23,7 +23,7 @@ const formSchema = z.object({
 });
 
 export const SubscribeForm = () => {
-  const [success, setSuccess] = useState(true);
+  const [success, setSuccess] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
