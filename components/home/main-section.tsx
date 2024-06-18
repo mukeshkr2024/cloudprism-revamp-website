2f0002402fae6020c58a8fc0f6c083272e12fc09
mukeshkr2024 <mukeshkumar@cloudprism.in>
2024-06-18 18:00:11 +0530

    chore: Update dependencies for GSAP and @gsap/react

--- components/home/main-section.tsx
+++ components/home/main-section.tsx
@@ -5,21 +5,32 @@ import React from "react";
 import CustomButton from "../shared/custom-button";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Spline = dynamic(() => import("@splinetool/react-spline"), {
   ssr: false,
 });
 
 function MainSection() {
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 0.5,
+      y: -30,
+    });
+  }, []);
+
   return (
     <div className="relative flex h-screen max-h-[1000px] w-full items-center justify-center ">
       <div className="size-full">
-        {/* <Spline scene="https://prod.spline.design/xboa1Cf0xqy0IBde/scene.splinecode" /> */}
-        {/* <Spline scene="https://prod.spline.design/2vZlQQ5DGv5WqORt/scene.splinecode" /> */}
         <Spline scene="https://prod.spline.design/wP5JM0Ppw0yvu-2A/scene.splinecode" />
       </div>
 
-      <div className="absolute flex max-w-4xl flex-col items-center justify-center gap-6  text-white">
+      <div
+        className="absolute mt-20 flex max-w-4xl flex-col items-center justify-center gap-6  text-white opacity-0"
+        id="hero"
+      >
         <h4 className="h1_bold">
           Create Value That <br /> Empowers
         </h4>
