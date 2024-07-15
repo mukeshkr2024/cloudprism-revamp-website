9c189065a5687dbfd27cac09c415aaaa4db23627
mukeshkr2024 <mukeshkumar@cloudprism.in>
2024-07-15 16:22:58 +0530

    feat: Update sitemap to include blog and news entries
    This commit adds code to the sitemap file to include blog and news entries in the sitemap. The blog entries are dynamically generated based on the allBlogs array, and the news entries are generated based on the allNews array. Each entry includes the URL and the last modified date.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,16 @@
+import { allBlogs, allNews } from "@/.contentlayer/generated";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const blogEntries = allBlogs.map((blog) => ({
+    url: `${process.env.BASE_URL}${blog.url}`,
+    lastModified: new Date(),
+  }));
+
+  const newsEntries = allNews.map((news) => ({
+    url: `${process.env.BASE_URL}${news.url}`,
+    lastModified: new Date(),
+  }));
   return [
     {
       url: `${process.env.BASE_URL}`,
@@ -38,5 +48,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${process.env.BASE_URL}/contact-us`,
       lastModified: new Date(),
     },
+    {
+      url: `${process.env.BASE_URL}/privacy-policy`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/terms-and-conditions`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/april-salesforce-meet`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/india-dreamin-2023`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/techsparks-2024`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/mc-square-2023`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/wit-summit-jaipur`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/mule-dreamin-2023`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/event/youth-dreamin-bihar-2024`,
+      lastModified: new Date(),
+    },
+    ...blogEntries,
+    ...newsEntries,
   ];
 }
