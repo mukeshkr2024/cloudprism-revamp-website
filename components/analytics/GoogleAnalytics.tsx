4d389508c211ac5b41e3eca1e2889fd0f98189a1
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-08-28 15:05:40 +0530

    add scripts

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -7,6 +7,7 @@ const GTM_ID = "GTM-NC7PNMT9";
 export const GoogleAnalytics = () => {
   return (
     <>
+      {/* Google Analytics script  */}
       <Script
         id="google-analytics"
         strategy="afterInteractive"
@@ -20,6 +21,43 @@ export const GoogleAnalytics = () => {
               j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+      {/* Additional  script */}
+      <Script
+        id="additional-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function () {
+              var reb2b = window.reb2b = window.reb2b || [];
+              if (reb2b.invoked) return;
+              reb2b.invoked = true;
+              reb2b.methods = ["identify", "collect"];
+              reb2b.factory = function (method) {
+                return function () {
+                  var args = Array.prototype.slice.call(arguments);
+                  args.unshift(method);
+                  reb2b.push(args);
+                  return reb2b;
+                };
+              };
+              for (var i = 0; i < reb2b.methods.length; i++) {
+                var key = reb2b.methods[i];
+                reb2b[key] = reb2b.factory(key);
+              }
+              reb2b.load = function (key) {
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.async = true;
+                script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
+                var first = document.getElementsByTagName("script")[0];
+                first.parentNode.insertBefore(script, first);
+              };
+              reb2b.SNIPPET_VERSION = "1.0.1";
+              reb2b.load("GOYPYHV3LZOX");
+            }();
           `,
         }}
       />
